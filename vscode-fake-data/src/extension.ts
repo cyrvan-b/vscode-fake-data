ec4fdc6610ac6ab5d6856bc41df23b1315d361c4
Cyrvan Bouchard <cyrvan.bouchard@gmail.com>
2023-11-13 22:27:42 +0100

    feat: add fullname command
    some of the previous code structure was also refactor to allow
    most of the commands definition to be minimal.
    
    issue: #
    changelog: skip

--- vscode-fake-data/src/extension.ts
+++ vscode-fake-data/src/extension.ts
@@ -3,11 +3,20 @@ import * as vscode from 'vscode';
 import { faker } from '@faker-js/faker';
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerTextEditorCommand('vscode-fake-data.fake.uuid', async (editor, edit) => {
-		editor.selections.forEach((selection) => {
-			edit.insert(selection.active, faker.string.uuid());
-		});
-	}));
+	const fakers: [any, any] = [
+		['vsf.fake.uuid', faker.string.uuid],
+		['vsf.fake.fullname', faker.person.fullName],
+	];
+
+	context.subscriptions.push(
+		...fakers.map(([name, func]) => {
+			return vscode.commands.registerTextEditorCommand(name, (editor, edit) => {
+				editor.selections.forEach((selection) => {
+					edit.insert(selection.active, func());
+				});
+			});
+		})
+	);
 }
 
 export function deactivate() {}
