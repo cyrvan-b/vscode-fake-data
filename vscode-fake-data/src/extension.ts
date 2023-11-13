8e01c733c919c138338367006e1d8b32608af8b1
Cyrvan Bouchard <cyrvan.bouchard@gmail.com>
2023-11-13 22:21:40 +0100

    refactor: use a more scalable way to define commands
    issue: #
    changelog: skip

--- vscode-fake-data/src/extension.ts
+++ vscode-fake-data/src/extension.ts
@@ -3,12 +3,11 @@ import * as vscode from 'vscode';
 import { faker } from '@faker-js/faker';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerTextEditorCommand('vscode-fake-data.fake.uuid', async (editor, edit) => {
+	context.subscriptions.push(vscode.commands.registerTextEditorCommand('vscode-fake-data.fake.uuid', async (editor, edit) => {
 		editor.selections.forEach((selection) => {
 			edit.insert(selection.active, faker.string.uuid());
 		});
-	});
-	context.subscriptions.push(disposable);
+	}));
 }
 
 export function deactivate() {}
