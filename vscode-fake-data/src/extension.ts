e1248a66b619f924963f7d5a9c9139a13dd3024c
Cyrvan Bouchard <cyrvan.bouchard@gmail.com>
2023-11-18 19:03:35 +0100

    feat: add commands
    add paragraph, words, and avatar generation
    
    issue: #
    changelog: skip

--- vscode-fake-data/src/extension.ts
+++ vscode-fake-data/src/extension.ts
@@ -6,13 +6,16 @@ export function activate(context: vscode.ExtensionContext) {
 	const fakers: [any, any] = [
 		['vsf.fake.uuid', faker.string.uuid],
 		['vsf.fake.fullname', faker.person.fullName],
+		['vsf.fake.paragraph', faker.lorem.paragraph],
+		['vsf.fake.words', faker.lorem.words],
+		['vsf.fake.avatar', faker.image.avatar],
 	];
 
 	context.subscriptions.push(
-		...fakers.map(([name, func]) => {
+		...fakers.map(([name, func, ...args]) => {
 			return vscode.commands.registerTextEditorCommand(name, (editor, edit) => {
 				editor.selections.forEach((selection) => {
-					edit.insert(selection.active, func());
+					edit.insert(selection.active, func(...args));
 				});
 			});
 		})
